952a6c55050acfae95c194c42f50c4ffbb7b8d3a
Steven Wanderski <steven@bxcreative.com>
2013-08-17 13:16:03 -0500

    homepage init

--- index.php
+++ index.php
@@ -6,44 +6,73 @@
 </head>
 
 <body class="front">
-	
+
 	<?php include('includes/header.php'); ?>
 
 	<section id="main"><div class="inner clearfix">
-		
-		<h1>We build <em>web</em> applications.</h1>
-		<p class="body">We are a small, Chicago four-man team that loves the web.<br />We provide all things needed to build a web application including user experience strategy,<br />design, and development.</p>
-		<p class="body cta"><a class="btn" href="/contact">Click here to build something cool with us</a></p>
 
-		<h2>Our Work</h2>
-		<section id="content">
-				<div class="project-wrap">
-					<a href="lightswitch">
-						<div class="project-image"><img src="/images/logo_ls.png"></div>
-						<div class="project-title">lightswitch.com</div>
-						<div class="project-tagline">Video management and approval system</div>
-					</a>
-				</div>
-				<div class="project-wrap middle">
-					<a href="cappellos">
-						<div class="project-image"><img src="/images/logo_cappy.png"></div>
-						<div class="project-title">cappellosglutenfree.com</div>
-						<div class="project-tagline">Online gluten-free pasta shop</div>
-					</a>
-				</div>
-				<div class="project-wrap">
-					<a href="emprego">
-						<div class="project-image"><img src="/images/logo_emprego.png"></div>
-						<div class="project-title">empregoligado.com.br</div>
-						<div class="project-tagline">SMS job marketplace</div>
-					</a>
-				</div>
-			</section>
+		<div class="content">
+			<h1>We make websites.</h1>
+
+			<div class="team">
+				<div class="member"><img src="/images/team/steven.png" alt="Steven Wanderski - Lead Programmer" /></div>
+				<div class="member"><img src="/images/team/brad.png" alt="Brad Sawicki - Front End Developer" /></div>
+				<div class="member"><img src="/images/team/phil.png" alt="Phil Weber - User Experience / Interaction Designer" /></div>
+				<div class="member"><img src="/images/team/dan.png" alt="Dan Leu - Designer" /></div>
+			</div>
+
+			<p class="body">
+				We are a four-man team based in Chicago that love building for the web.<br>
+				Since we are small, we like working with small teams. Let's chat if you are:
+			</p>
+
+			<div class="list-wrap clearfix">
+				<ul>
+					<li>A startup with a web application idea</li>
+					<li>A small team that needs a new website built or designed</li>
+					<li>A team that needs an existing site redesigned and built</li>
+				</ul>
+			</div>
+
+			<p class="body cta"><a href="/contact">Contact Us About a Project</a></p>
+		</div>
 
 	</div></section><!-- end #main -->
 
+	<div class="work"><div class="inner clearfix">
+		<section id="content">
+			<div class="project-wrap">
+				<a href="lightswitch">
+					<div class="project-image">
+						<img src="/images/projects/logos/smas.jpg">
+					</div>
+					<div class="project-title">singmeastory.org</div>
+					<div class="project-tagline">Nonprofit organization helping children</div>
+				</a>
+			</div>
+			<div class="project-wrap middle">
+				<a href="cappellos">
+					<div class="project-image">
+						<img src="/images/projects/logos/thrifted.png">
+					</div>
+					<div class="project-title">thriftedandmodern.com</div>
+					<div class="project-tagline">Online vintage clothing shop</div>
+				</a>
+			</div>
+			<div class="project-wrap">
+				<a href="emprego">
+					<div class="project-image">
+						<img src="/images/projects/logos/emprego.png">
+					</div>
+					<div class="project-title">empregoligado.com.br</div>
+					<div class="project-tagline">SMS job marketplace</div>
+				</a>
+			</div>
+		</section>
+	</div></div>
+
 	<?php include('includes/footer.php'); ?>
-	
+
 </body>
 
 </html>
